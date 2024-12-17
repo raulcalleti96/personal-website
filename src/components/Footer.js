b7dd987825890117d5097f29b4786a4a3e31f9df
raulsantiago <raul.santiago@elastic.co>
2024-12-17 14:47:41 +0100

    Initial structure and README

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>© 2024 Raúl Santiago Prieto. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
